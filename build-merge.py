cf108c9ac2d241a56988967b99d3c69f8f56dbca
Alper Yazar <alperyazar@gmail.com>
2021-02-10 07:32:23 +0300

    remove residuals from mkdocs and fix axseq number of first post of 2021 (#98)

--- build-merge.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-# This script build the site for merge request checking
-
-import subprocess
-
-subprocess.run(["pipenv", "install"])
-subprocess.run(["pipenv", "run", "mkdocs", "build", "--clean", "--strict", "--verbose", "--config-file", "mkdocs-merge.yml"], check=True)
--- docs/contact.md
+++
@@ -1,9 +0,0 @@
----
-date: 2020-11-07
-disqus: ''
----
-
-# Contact us
-
-!!! todo
-    Write
