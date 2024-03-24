b06c8e25f93072b75cfb4d83d7fe24fa966c63d0
alperyazar <>
2024-03-24 03:14:09 +0000

    Deploying to gh-pages from @ asynx-dev/www@3075474694600e518e5543274934bec6356005f1 🚀

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,85 @@
+var store = [{
+        "title": "MIG \"init_calib_complete\" signal does not go high",
+        "excerpt":"For a project, which utilizes 7-Series, I should use PL memory and MIG is the best IP choice for this purpose. In many websites, including Xilinx documents, you can find how to configure MIG IP. Even if there are many configuration parameters in MIG IP, if you know where to...","categories": ["solution"],
+        "tags": ["en","xilinx","zynq","mig"],
+        "url": "/blog/20/10/mig-calibration-error-xilinx.html",
+        "teaser": null
+      },{
+        "title": "Use pip and pipenv with self signed SSL certificates",
+        "excerpt":"If you work in a cooperate environment, you may be working behind proxies and SSL traffic may be signed by self-signed certificates. This may cause problems while working with pip and especially with pipenv. With current versions of pip, it is possible to ignore SSL errors using the following example...","categories": ["guide"],
+        "tags": ["en","pip","pipenv","proxy","ssl"],
+        "url": "/blog/20/11/using-pip-and-pipenv-with-self-signed-certificates.html",
+        "teaser": null
+      },{
+        "title": "Using VS Code with self signed SSL certificates behind proxy",
+        "excerpt":"Similar to the case I mentioned in my previous post, you may want to use VS Code behind a proxy which also signs SSL traffic with a self signed certificate. In that case, Internet related features of the VS Code like Extensions Marketplace won’t work. Solution SSL certificate If we...","categories": ["guide"],
+        "tags": ["en","vscode","proxy","ssl"],
+        "url": "/blog/20/11/using-vscode-with-self-signed-certificates-behind-proxy.html",
+        "teaser": null
+      },{
+        "title": "MII signal bug in Xilinx FPGA",
+        "excerpt":"The main goal of this article to give the general information about Ethernet packets and MII concept, and demonstrate a bug that I have recently faced while working on MII signals in Vivado, with the architecture that I build. General information The MII (Media-Independent Interface) was defined as a standard...","categories": ["solution"],
+        "tags": ["en","xilinx","mii","ethernet","zynq"],
+        "url": "/blog/20/11/mii-signal-bug-in-xilinx-fpga.html",
+        "teaser": null
+      },{
+        "title": "Serial data communication over TCP",
+        "excerpt":"Why would you need serial over ethernet ? A serial device on remote PC can be viewed as directly connected to your PC with serial over ethernet. Two applications on different PCs talk over serial ports as if applications directly connected through serial ports. This could be sensible if you...","categories": ["guide"],
+        "tags": ["en","socat","uart","serial","remote"],
+        "url": "/blog/20/11/serial-data-communication-over-tcp.html",
+        "teaser": null
+      },{
+        "title": "Getting started to Vitis acceleration flow with Zynq 7000",
+        "excerpt":"We all know, sometimes it is just hard to get started learning things from vendor documentations. This tutorial will follow beginner friendly steps to run your first accelerator on an FPGA. This tutorial will be more beneficial when used in conjunction with the official Xilinx tutorial1 published on github. There...","categories": ["guide"],
+        "tags": ["en","xilinx","vitis","zynq"],
+        "url": "/blog/20/12/getting-started-to-vitis-acceleration-flow-with-zynq-7000.html",
+        "teaser": null
+      },{
+        "title": "Jekyll'ye geçiş",
+        "excerpt":"Jekyll’den merhaba! 7 Şubat 2021’de siteyi Ruby temelli olan Jekyll‘ye taşıdım, eskiden Python temelli olan MkDocs kullanıyorduk. Peki bu okuyucular için ne ifade ediyor? Aslında pek bir değişiklik yok okuyanlar için. Daha çok blog yazıları yazıldığı için Jekyll bu işler için daha uygun (kullandığımız Minimal Mistakes teması ile berber), dahili...","categories": ["news"],
+        "tags": ["tr","jekyll","mkdocs"],
+        "url": "/blog/21/2/jekyll.html",
+        "teaser": "/assets/images/blog/21/1.png"
+      },{
+        "title": "Using WSL on an offline Windows machine",
+        "excerpt":"In this post, we will install Ubuntu 20.04 WSL package on an offline Windows 10 machine. Although this post is prepared with Ubuntu 20.04, the steps should be valid for other WSL packages too. Getting files If your Windows machine is connected to the Internet and you can easily install...","categories": ["guide"],
+        "tags": ["en","offline","wsl","windows","ubuntu"],
+        "url": "/blog/21/3/wsl-offline.html",
+        "teaser": "/assets/images/blog/21/2.jpg"
+      },{
+        "title": "Raspberry Pi SD kart taşıma ve büyütme",
+        "excerpt":"Raspberry Pi 400’de kullandığım SD kartın boyutu biraz küçük kalmaya başlayınca daha büyük bir SD karta (performansı da biraz daha iyi olabilir) geçmem gerekti. Bu yazıyı küçük bir not yazısı olarak düşünebilirsiniz. İnternette farklı yöntemlerle bu işi tarifleyen birçok yazı bulabilirsiniz. Yapılan işlemin çoğu RPi 400’e özel olmadığından diğer RPi’ler...","categories": ["guide"],
+        "tags": ["tr","raspberry-pi","sd","raspbian","raspberry-pi-os"],
+        "url": "/blog/21/3/rpi-sd-kart-buyutme.html",
+        "teaser": "/assets/images/blog/21/3_teaser.jpg"
+      },{
+        "title": "Sumatra PDF",
+        "excerpt":"Giriş Sumatra PDF, Windows için geliştirilmiş küçük boyutlu, hafif ve hızlı açılan bir PDF görüntüleme yazılımı. Sanılabileceğinin aksine yazılım, adını Endonezya’nın Sumatra Adası’ndan almıyor 1. GPLv3 ile lisanslanmış olan yazılım, Github üzerinden açık kaynak ve ücretsiz olarak sunuluyor. Bu yazılımı, Windows üzerinde çalışırken günlük yaşantımda sıklıkla kullanıyorum. Bu yazıda, yazılımı...","categories": ["guide","podcast","youtube"],
+        "tags": ["tr","sumatrapdf","windows","latex","synctex"],
+        "url": "/blog/21/10/sumatrapdf.html",
+        "teaser": "/assets/images/blog/21/5_art.png"
+      },{
+        "title": "Create search link between SumatraPDF and TeXstudio with SyncTeX",
+        "excerpt":"This post was originally published on Alper Yazar’s personal blog in 2014. I use TeXstudio for my LaTeX works. SumatraPDF is also good, lightweight PDF reader. It is possible to create a forward search from TeXstudio to SumatraPDF. You may find PDF output of a LaTeX code. Similarly, a backward...","categories": ["guide"],
+        "tags": ["en","sumatrapdf","texstudio","latex","synctex"],
+        "url": "/blog/21/10/sumatrapdf-texstudio-synctex-search-link.html",
+        "teaser": "/assets/images/blog/21/6_teaser.png"
+      },{
+        "title": "WinDirStat",
+        "excerpt":"https://windirstat.net/ Açık kaynak Windows WinDirStat, açılımı ile Windows Directory Statistics, özellikle diskim dolmaya yaklaştığı zaman kullandığım bir yazılım. Çünkü böyle durumlarda bir kurban seçip onu silmem gerekiyor. Bir noktadan sonra hangi klasörün içinde gerekli gereksiz ya da büyük küçük hangi dosyalar var insan unutuyor. Bu yazılım ile bu tarz klasörleri...","categories": ["guide"],
+        "tags": ["tr","windirstat"],
+        "url": "/blog/22/1/windirstat.html",
+        "teaser": "/assets/images/blog/22/1_teaser.png"
+      },{
+        "title": "Running PetaLinux on an offline machine",
+        "excerpt":"If you are a Xilinx customer and want to compile an embedded Linux with PetaLinux on an offline machine, you can do it by mirroring Xilinx repos and configuring your project. Even if you are connected to the Internet, you may want to do this because: You will be make...","categories": ["guide"],
+        "tags": ["en","petalinux","xilinx"],
+        "url": "/blog/22/2/petalinux-offline.html",
+        "teaser": "/assets/images/blog/22/2_teaser.png"
+      },{
+        "title": "C programlamada gördüğüm en sık yapılan hata",
+        "excerpt":"Gömülü sistemlerle uğraşan biri olarak C dili ile ilgileniyorum. Bu yazıda oldukça masum duran fakat başımızı derde sokabilecek bir kodlama hatasından bahsetmek istiyorum. Yeni öğrenen kişilerin kodlarında (dikkatsizlik durumunda görece tecrübeli kişilerinkilerde bile) karşıma en sık çıkan C programlama hatası bu. İlk olarak hatalı olmayan bir duruma bakalım. Bir fonksiyonun...","categories": ["guide"],
+        "tags": ["tr","c-kafe"],
+        "url": "/blog/22/6/c-programlamada-en-cok-yapilan-hata.html",
+        "teaser": "/assets/images/blog/22/3.png"
+      }]
